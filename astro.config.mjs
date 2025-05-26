573ec9343b6e80a8c5b36f59598af5163f9a1746
Josh <josh.e.stein@gmail.com>
2025-05-26 01:59:18 +0100

    Remove `astro-shield`
    Try as I might I couldn't get CSP headers to be set correctly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 // @ts-check
-import { resolve } from "node:path";
-
 import mdx from "@astrojs/mdx";
-import { shield } from "@kindspells/astro-shield";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
-const rootDir = new URL(".", import.meta.url).pathname;
-const modulePath = resolve(rootDir, "src", "generated", "sriHashes.mjs");
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,25 +9,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [
-    mdx(),
-    shield({
-      sri: {
-        enableMiddleware: true, // MUST be enabled for dynamic pages!
-        hashesModule: modulePath, // SHOULD be set!
-      },
-      securityHeaders: {
-        contentSecurityPolicy: {
-          // cspDirectives: {
-          // "default-src": "'self'",
-          // "style-src": "'self'",
-          // "font-src": "'self' data:",
-          // "base-uri": "'self'",
-          // "frame-ancestors": "'none'",
-          // "script-src": "'self'",
-          // },
-        },
-      },
-    }),
-  ],
+  integrations: [mdx()],
 });

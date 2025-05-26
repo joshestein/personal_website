bb3bd1c5453c49a007e080c43724694166133b48
Josh <josh.e.stein@gmail.com>
2025-05-26 01:21:35 +0100

    Attempt to set CSP directives in code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
+import { resolve } from "node:path";
+
 import mdx from "@astrojs/mdx";
 import { shield } from "@kindspells/astro-shield";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+
+const rootDir = new URL(".", import.meta.url).pathname;
+const modulePath = resolve(rootDir, "src", "generated", "sriHashes.mjs");
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +15,25 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), shield({})],
+  integrations: [
+    mdx(),
+    shield({
+      sri: {
+        enableMiddleware: true, // MUST be enabled for dynamic pages!
+        hashesModule: modulePath, // SHOULD be set!
+      },
+      securityHeaders: {
+        contentSecurityPolicy: {
+          cspDirectives: {
+            "default-src": "'self'",
+            "style-src": "'self'",
+            "font-src": "'self' data:",
+            "base-uri": "'self'",
+            "frame-ancestors": "'none'",
+            "script-src": "'self'",
+          },
+        },
+      },
+    }),
+  ],
 });
